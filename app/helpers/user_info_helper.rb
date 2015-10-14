0d1bbe442ad222e9c54f0c28b8e1145d1158c168
kkennis <kevinvkennis@gmail.com>
2015-10-14 16:06:57 -0700

    Add full name info to user

--- app/helpers/user_info_helper.rb
+++ app/helpers/user_info_helper.rb
@@ -3,6 +3,8 @@ module UserInfoHelper
   def inject_extra_userprops(user)
     user_as_json = JSON.parse(user.to_json)
 
+
+    user_as_json["full_name"] = user["first_name"] + " " + user["last_name"]
     user_as_json["votes"] = JSON.parse(user.votes.to_json) 
   end
 
