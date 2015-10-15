c05694c23975e510a7eb5ef00d7f13ed3702b2c9
Stephanie Hutson <email": null>
2015-10-15 08:33:28 -0700

    remove devise. database seedable with bcrypt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,7 @@ class ApplicationController < ActionController::API
   # protect_from_forgery with: :null_session
   # before_action :authenticate_user!
 
-  before_action :configure_sign_up_params, only: [:create], if: :devise_controller?
-  before_action :configure_account_update_params, only: [:update], if: :devise_controller?
+
 
   before_action :allow_cross_origin_requests, if: proc { Rails.env.development? }
 
@@ -30,21 +29,6 @@ class ApplicationController < ActionController::API
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.for(:sign_up) << :first_name
-    devise_parameter_sanitizer.for(:sign_up) << :last_name
-  end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.for(:account_update) << :first_name
-    devise_parameter_sanitizer.for(:account_update) << :last_name
-    devise_parameter_sanitizer.for(:account_update) << :phone
-    devise_parameter_sanitizer.for(:account_update) << :blurb
-    devise_parameter_sanitizer.for(:account_update) << :user_type
-    devise_parameter_sanitizer.for(:account_update) << :company
-    devise_parameter_sanitizer.for(:account_update) << :picture_url
-  end
 
 
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+  respond_to :html, :json
+  include UserInfoHelper
+  before_action :get_current_user
+
+  def show
+    user = User.where(id, parmas[:id])
+    user.inject_extra_user_props(user)
+    render json: user
+  end
+
+  # def edit
+
+  # end
+
+  def login
+    p params
+
+    current_user = User.find_by_email(params[:email])
+    if current_user.password == params[:password]
+      render json: current_user
+    else
+      # redirect_to home_url
+    end
+  end
+
+  def logout
+
+  end
+
+  # def new
+
+  # end
+
+  def create
+    p params
+
+    user = User.new(params[:user])
+    user.password = params[:password]
+    user.save!
+
+  end
+
+  def destroy
+
+  end
+
+  def update
+
+  end
+
+  def get_current_user
+    current_user = User.find
+  end
+
+end
