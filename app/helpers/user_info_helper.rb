b1d869b73f40fa429522e612e676a573adc54420
kkennis <kevinvkennis@gmail.com>
2015-10-14 17:02:37 -0700

    Add more vote information to user json

--- app/helpers/user_info_helper.rb
+++ app/helpers/user_info_helper.rb
@@ -1,11 +1,36 @@
 module UserInfoHelper
+  include PitchHelper
 
   def inject_extra_user_props(user)
     user_as_json = JSON.parse(user.to_json)
 
 
     user_as_json["full_name"] = user["first_name"] + " " + user["last_name"]
-    user_as_json["votes"] = JSON.parse(user.votes.to_json) 
+    user_as_json["votes"] = JSON.parse(user.votes.to_json)
+
+    user_as_json["votes"].map! do |vote|
+      vote_obj = Vote.find(vote["id"])
+
+      if vote_obj.votable_type == "Pitch"
+        vote["votable_title"] = vote_obj.votable.title
+      else
+        vote["votable_title"] = vote_obj.votable.pitch.title
+      end
+
+      vote
+    end
+
+    user_as_json["pitches"] = JSON.parse(user.pitches.to_json) 
+    user_as_json["pitches"].map! do |pitch|
+      pitch["vote_count"] = Pitch.find(pitch["id"]).votes.count
+      pitch
+    end
+    user_as_json["comments"] = JSON.parse(user.comments.to_json)
+    user_as_json["comments"].map! do |comment|
+      comment["vote_count"] = Comment.find(comment["id"]).votes.count
+      comment["pitch_title"] = Comment.find(comment["id"]).pitch.title
+      comment
+    end
 
     user_as_json
   end
